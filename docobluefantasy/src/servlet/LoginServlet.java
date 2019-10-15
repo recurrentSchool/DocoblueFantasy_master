2eb5c395ad46a308173c849e3ebe482a58ef9e58
6D167 <6D167@DESKTOP-EVTU2M5>
2019-10-15 16:05:55 +0900

    修理完了したLoginServlet

--- docobluefantasy/src/servlet/LoginServlet.java
+++ docobluefantasy/src/servlet/LoginServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,18 +12,17 @@ import javax.servlet.http.HttpSession;
 import bean.User;
 import model.LoginLogic;
 
-
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}//doGET
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
 		String name = request.getParameter("name");
@@ -35,19 +33,17 @@ public class LoginServlet extends HttpServlet {
 		User user = new User(name, pass);
 
 		HttpSession session = request.getSession();
-		session.setAttribute("message",message );
+		session.setAttribute("message", message);
 
 		LoginLogic bo = new LoginLogic();
 
 		boolean result = bo.executeLogin(user);
 
-
-
 		if (result == true) {
 
-			session.setAttribute("user",user );
+			session.setAttribute("user", user);
 
-			if(user.getAdmin() == 0) {
+			if (user.getAdmin() == 0) {
 
 				url = "/WEB-INF/admin.jsp";
 
@@ -55,15 +51,14 @@ public class LoginServlet extends HttpServlet {
 
 				url = "/main.jsp";
 
-		}
-		else {
+			} else {
 
+				message = "パスワードもしくは名前が違います";
+				request.setAttribute("message", message);
+				url = "/login.jsp";
+			}
 
-			message ="パスワードもしくは名前が違います";
-			request.setAttribute("message",message );
-			url = "/login.jsp";
-		}
+		} //doPOST
 
-	}//doPOST
-
+	}
 }
